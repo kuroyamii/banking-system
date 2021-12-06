98f6aa0ac6416c14ac4c250a66384951568be6ff
kuroyamii <gerysastrawan123@gmail.com>
2021-12-06 12:54:21 +0800

    tidy the main driver

--- App/main.c
+++ App/main.c
@@ -3,20 +3,47 @@
 
 int choice;
 char id[8];
+void header();
+void pause();
+void trxWithdraw();
+void trxDeposit();
+void trxHistory();
+void menu();
 
-//Function Clear Screen Cross-Platform.
-void clear(){
-    #ifdef _WIN32
-    //Windows
-        std:system("cls");
-    #else
-    //POSIX (Unix), etc
-        std:system("clear");
-    #endif
+//Main Driver To Run The Program
+int main()
+{
+main:
+
+    clear();
+    printf("(NAMABANK) Banking System");
+    printf("\n\n1. Login\n2. Sign Up\n3. Exit");
+    printf("\n\nSelect Action: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        //login();
+        break;
+    case 2:
+        //signup();
+        break;
+    case 3:
+        goto end;
+        break;
+    default:
+        pause;
+        goto main;
+        break;
+    }
+
+end:
+    return 0;
 }
 
 //Function Header Program (CLI)
-void header(){
+void header()
+{
     clear();
     printf("-----------------------------------------\n");
     printf("|       (NAMABANK) Banking System       |\n");
@@ -25,18 +52,58 @@ void header(){
     printf("-----------------------------------------\n");
 }
 
-void pause(){
+void pause()
+{
     printf("\nPilihan Tidak Tersedia.");
     printf("\nTekan ENTER untuk kembali...");
     fflush(stdin);
     getchar();
 }
- 
-void menu(){
+void trxWithdraw()
+{
+    //TBA
+}
+
+void trxDeposit()
+{
+    //TBA
+}
+
+void trxHistory()
+{
+    printf("\n1. Nominal Terkecil Ke Terbesar");
+    printf("\n2. Nominal Terbesar Ke Terkecil");
+    printf("\n3. Default\n4. Kembali");
+    printf("\n\nMasukkan Pilihan: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        //TBA
+        break;
+    case 2:
+        //TBA
+        break;
+    case 3:
+        //TBA
+        break;
+    case 4:
+        menu();
+    default:
+        pause();
+        trxHistory();
+        break;
+    }
+}
+
+void menu()
+{
     header();
     printf("\n1. Tarik Tunai\n2. Setor Tunai\n3. Riwayat Transaksi\n4. Logout");
-    printf("\n\nMasukkan Pilihan: "); scanf("%d", &choice);
-    switch (choice){
+    printf("\n\nMasukkan Pilihan: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
     case 1:
         header();
         trxWithdraw();
@@ -62,72 +129,4 @@ void menu(){
         menu();
         break;
     }
-}
-
-void login(){
-    //TBA
-}
-
-void signup(){
-    //TBA
-}
-
-void trxWithdraw(){
-    //TBA
-}
-
-void trxDeposit(){
-    //TBA
-}
-
-void trxHistory(){
-    printf("\n1. Nominal Terkecil Ke Terbesar");
-    printf("\n2. Nominal Terbesar Ke Terkecil");
-    printf("\n3. Default\n4. Kembali");
-    printf("\n\nMasukkan Pilihan: "); scanf("%d", &choice);
-    switch (choice){
-    case 1:
-        //TBA
-        break;
-    case 2:
-        //TBA
-        break;
-    case 3:
-        //TBA
-        break;
-    case 4:
-        menu();
-    default:
-        pause();
-        trxHistory();
-        break;
-    }
-}
-
-//Main Driver To Run The Program
-int main(){
-    main:
-    trxHistory();
-    clear();
-    printf("(NAMABANK) Banking System");
-    printf("\n\n1. Login\n2. Sign Up\n3. Exit");
-    printf("\n\nSelect Action: "); scanf("%d", &choice);
-    switch (choice){
-    case 1:
-        login();
-        break;
-    case 2:
-        signup();
-        break;
-    case 3:
-        goto end;
-        break;
-    default:
-        pause;
-        goto main;
-        break;
-    }
-
-    end:
-    return 0;
 }
